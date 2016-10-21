44b0d4038395cae76fc3bc19ea9c0be5590406b2
davejoshmike <davejoshmike@gmail.com>
2016-10-21 16:45:34 -0400

    Initial Commit

---
+++ Meerkat/Meerkat/MeerkatAI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MeerkatAI
+{
+    class MeerkatAI
+    {
+        static void Main(string[] args)
+        {
+            //Calls timeOutHandler after 9.5 seconds
+            Timer time = new Timer();
+            time.Elapsed += new ElapsedEventHandler(timeOutHandler);
+            time.Interval = (10*1000)-500;
+            time.Enabled = true; //start timer
+
+            Console.Read(); //exit on input
+        }
+
+        private static void timeOutHandler(object sender, ElapsedEventArgs e)
+        {
+            //TODO Implement logic that chooses a move at timeout
+            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+
+            switch (rand.Next(0,11))
+            {
+                case 0:
+                case 1:
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+    }
+}
