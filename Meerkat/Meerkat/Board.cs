b2c3bf94b686181544e2b8bd86e06403f3e9902b
davejoshmike <davejoshmike@gmail.com>
2016-10-22 15:49:04 -0400

    Finished up Horizontal Goal for the Heuristic

--- Meerkat/Meerkat/Board.cs
+++ Meerkat/Meerkat/Board.cs
@@ -12,8 +12,15 @@ namespace MeerkatAI
         private static readonly log4net.ILog log =
     log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private int[,] myBoard;
+        private int player;
+        private int p1threes;
+        private int p1fours;
+        private int p2threes;
+        private int p2fours;
         private int TOP = 0;
         private int BOTTOM = 5;
+        private int NUM_OF_ROWS = 6;
+        private int NUM_OF_COLUMNS = 7;
 
         public Board(string board, int player)
         {
@@ -27,6 +34,7 @@ namespace MeerkatAI
             {
                 log.Error(e);
             }
+            this.player = player;
 
             
         }
@@ -34,7 +42,7 @@ namespace MeerkatAI
         public int[] ValidMoves()
         {
             var list = new List<int>();
-            for (int i = 0; i < 7; i++)
+            for (int i = 0; i < NUM_OF_COLUMNS; i++)
             {
                 if (myBoard[0, i] == 0)
                 {
@@ -46,8 +54,31 @@ namespace MeerkatAI
 
         public int Heuristic()
         {
-            //Defense has a higher heuristic
-            return -1;
+            var score = 0;
+            p1threes = 0;
+            p1fours = 0;
+            p2threes = 0;
+            p2fours = 0;
+            for (int x = 0; x < NUM_OF_COLUMNS; x++)
+            {
+                for (int y = 0; y < NUM_OF_ROWS; y++)
+                {
+                    if (myBoard[y, x] != 0)
+                    {
+                        //Found a piece!
+                        HorizontalGoal(x, y - 1, 1);
+                        HorizontalGoal(x, y - 1, 2);
+                        //VerticalGoal(x, y);
+                    }
+                }
+            }
+
+            score += (p1threes - p2threes);
+            score += (p1fours - p2fours)*10;
+            //score needs to negative if player 2
+            if(player==2)
+                score *= -1;
+            return score;
         }
 
         public int NumPieces()
@@ -57,12 +88,20 @@ namespace MeerkatAI
 
         public bool IsGoal()
         {
-            return false;
+            myBoard.ToString();
+            for (int i = 0; i < NUM_OF_ROWS; i++)
+            {
+                for (int j = 0; j < NUM_OF_COLUMNS; j++)
+                {
+                    
+                }
+            }
+            return true;
         }
 
         public bool IsEmpty()
         {
-            for (int i = 0; i < 7; i++)
+            for (int i = 0; i < NUM_OF_COLUMNS; i++)
             {
                 if (myBoard[BOTTOM, i] != 0)
                 {
@@ -81,6 +120,59 @@ namespace MeerkatAI
         private bool DiagonalGoal()
         {
             
+            return false;
+        }
+
+        private void HorizontalGoal(int x, int y, int whichPlayer)
+        {
+            int xcoord = x;
+            int pieceCount = 1;
+            //left
+            while (xcoord>0)
+            {
+                
+                xcoord--;
+                if (myBoard[y, xcoord] == whichPlayer)
+                {
+                    pieceCount++;
+                }
+                else{
+                    break;
+                }
+            }
+            //right
+            while (xcoord<7)
+            {
+                xcoord++;
+                if (myBoard[y, xcoord] == whichPlayer)
+                {
+                    pieceCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (pieceCount == 3)
+            {
+                if(whichPlayer==1)
+                    p1threes++;
+                if (whichPlayer == 2)
+                    p2threes++;
+            }
+            if (pieceCount == 4)
+            {
+                if (whichPlayer == 1)
+                    p1fours++;
+                if (whichPlayer == 2)
+                    p2fours++;
+            }
+        }
+
+        private bool VerticalGoal()
+        {
+
             return false;
         }
     }
